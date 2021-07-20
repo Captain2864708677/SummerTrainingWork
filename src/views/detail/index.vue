bfe307b84636b1a0da27c9f6c88b119806c05b07
piggy-piggy-piggy <1220762972@qq.com>
2021-07-20 16:17:03 +0800

    购物车前端(测试分支张颖)

---
+++ src/views/detail/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div>
+        代表商品id{{productId}}欢迎你
+        <van-button @click="close()" color=red size="small">关闭</van-button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "index",
+        props:{
+            productId:'',
+        },
+        methods:{
+            close(){
+                this.$emit('update:visible',false)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
